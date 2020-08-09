ef0dce151393926b11c5920db8a9375d0f7cfb10
vedanttttt <vatheking@gmail.com>
2020-08-09 16:09:45 +0530

    added about us page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import TourDetail from './TourdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import {TOURS} from '../shared/tours';
 import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
@@ -50,6 +51,7 @@ class Main extends Component {
       	<Route exact path="/pamplet" component={()=> <Pamplet tours={this.state.tours} /> } />
         <Route path="/menu/:dishId" component={TourWithId} />
         <Route exact path="/contactus" component={Contact} />
+        <Route exact path="/aboutus" component={()=> <About leaders={this.state.leaders} /> } />
       	<Redirect to="/home" />
       </Switch>
       <Footer />
